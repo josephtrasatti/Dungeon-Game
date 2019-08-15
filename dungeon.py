9037b027d9cd003b3c630548e0d7a83cba754c4f
jtrasatt <jt3183@columbia.edu>
2019-08-15 19:25:23 -0400

    Update dungeon.py

--- dungeon.py
+++ dungeon.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-display_width = 800
-display_height = 600
+display_width = 400
+display_height = 300
 character_width = 50
 
 black = (0, 0, 0)
@@ -15,7 +15,7 @@ green = (0, 0, 255)
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Dungeon Game')
 clock = pygame.time.Clock()
-personImg = pygame.image.load('person.png')
+personImg = pygame.image.load('robot.png')
 
 
 def character(x, y):
@@ -24,7 +24,7 @@ def character(x, y):
 
 def game_loop():
     x = (display_width * 0.45)
-    y = (display_height * 0.8)
+    y = (display_height * 0.6)
     x_change = 0
     y_change = 0
     game_exit = False
@@ -39,12 +39,19 @@ def game_loop():
                     x_change = -5
                 if event.key == pygame.K_RIGHT:
                     x_change = 5
+                if event.key == pygame.K_UP:
+                    y_change = -5
+                if event.key == pygame.K_DOWN:
+                    y_change = 5
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_change = 0
 
         x += x_change
+        y += y_change
 
         gameDisplay.fill(white)
         character(x, y)
@@ -53,8 +60,7 @@ def game_loop():
             game_exit = True
 
         pygame.display.update()
-        clock.tick(60)
-        print(clock.get_fps())
+        clock.tick(30)
 
 
 game_loop()
